1d6f06c5f7c4dbc0420dea882eccc78344731d47
Antonis Poulakis <antwnic4@gmail.com>
2019-08-18 22:07:14 +0300

    changed the consumer-producer to use promises (async-await) instead of callbacks

--- rabbitmq/receive.js
+++ rabbitmq/receive.js
@@ -1,44 +1,87 @@
 #!/usr/bin/env node
 
-const amqp = require("amqplib/callback_api");
+const open = require("amqplib").connect("amqp://localhost");
 
-amqp.connect("amqp://localhost", function(error0, connection) {
-  if (error0) {
-    throw error0;
-  }
-  connection.createChannel(function(error1, channel) {
-    if (error1) {
-      throw error1;
-    }
+const queue = "transactions";
 
-    const queue = "transactions";
-
-    channel.assertQueue(queue, {
+// Consumer
+open
+  .then(async conn => {
+    const channel = await conn.createChannel();
+    const ok = await channel.assertQueue(queue, {
       durable: false
     });
 
-    console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
-
-    channel.consume(
-      queue,
-      (msg) => {
-        // let item = {};
-        try {
-          const transaction = JSON.parse(msg.content.toString());
-          const tokens = transaction.size * 10;
-          const deliverable = {
-            hash: transaction.hash,
-            tokens
+    if (ok) {
+      console.log(
+        " [*] Waiting for messages in %s. To exit press CTRL+C",
+        queue
+      );
+      channel.consume(
+        queue,
+        msg => {
+          // let item = {};
+          try {
+            const transaction = JSON.parse(msg.content.toString());
+            const tokens = transaction.size * 10;
+            const deliverable = {
+              hash: transaction.hash,
+              tokens
+            };
+            console.log(" [x] Received %s", deliverable);
+          } catch (e) {
+            console.log(e);
           }
-          console.log(" [x] Received %s", deliverable);
-
-        } catch(e) {
-          console.log(e);
+        },
+        {
+          noAck: true
         }
-      },
-      {
-        noAck: true
-      }
-    );
-  });
-});
+      );
+      return Promise.resolve();
+    } else {
+      return Promise.reject();
+    }
+  })
+  .catch(console.warn);
+
+// const amqp = require("amqplib/callback_api");
+
+// amqp.connect("amqp://localhost", (error0, connection) => {
+//   if (error0) {
+//     throw error0;
+//   }
+//   connection.createChannel((error1, channel) => {
+//     if (error1) {
+//       throw error1;
+//     }
+
+//     const queue = "transactions";
+
+//     channel.assertQueue(queue, {
+//       durable: false
+//     });
+
+//     console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+
+//     channel.consume(
+//       queue,
+//       msg => {
+//         // let item = {};
+//         try {
+//           const transaction = JSON.parse(msg.content.toString());
+//           const tokens = transaction.size * 10;
+//           const deliverable = {
+//             hash: transaction.hash,
+//             tokens
+//           };
+//           console.log(" [x] Received %s", deliverable);
+//         } catch (e) {
+//           console.log(e);
+//         }
+//       },
+//       {
+//         noAck: true
+//       }
+//     );
+//   });
+// });
--- rabbitmq/send.js
+++ rabbitmq/send.js
@@ -1,49 +1,39 @@
 #!/usr/bin/env node
 
-const amqp = require("amqplib/callback_api");
+const open = require("amqplib").connect("amqp://localhost");
 const blockexplorer = require("blockchain.info/blockexplorer").usingNetwork(3);
 const _ = require("lodash");
 
-amqp.connect("amqp://localhost", (error0, connection) => {
-  if (error0) {
-    throw error0;
-  }
-  connection.createChannel(async (error1, channel) => {
-    if (error1) {
-      throw error1;
-    }
-    const queue = "transactions";
+const queue = "transactions";
 
-    const addresses = [
-      "2MwcVyhWzUrS6AK8mwAZPjBfeQqtdEGgeSg",
-      "2NE7cEdVPFPrVviMvnQYGiNfeTMZnokHu23",
-      "2MtcSCijSWDruTq316PNV8onKzeq4amMPb3",
-      "2N2u2dMHKr4B7CMPQNptxEqrGWu9fVfCA5m",
-      "2N2C6oBBvrC8Kan4bREcvPQHLUE9FpgCY6g"
-    ];
-
-    const items = await blockexplorer.getMultiAddress(addresses);
-
-    // console.log(res);
-    // const items = res.txs.map((item, key) => item.hash);
-
-    // console.log(items);
-
-    channel.assertQueue(queue, {
+open
+  .then(async conn => {
+    const channel = await conn.createChannel();
+    const ok = await channel.assertQueue(queue, {
       durable: false
     });
+    if (ok) {
+      const addresses = [
+        "2MwcVyhWzUrS6AK8mwAZPjBfeQqtdEGgeSg",
+        "2NE7cEdVPFPrVviMvnQYGiNfeTMZnokHu23",
+        "2MtcSCijSWDruTq316PNV8onKzeq4amMPb3",
+        "2N2u2dMHKr4B7CMPQNptxEqrGWu9fVfCA5m",
+        "2N2C6oBBvrC8Kan4bREcvPQHLUE9FpgCY6g"
+      ];
 
-    _.forEach(items.txs, (item, index) => {
-      channel.sendToQueue(queue, Buffer.from(JSON.stringify(item)));
-      console.log(" [x] Sent transaction in index: %d", index);
-    });
+      const { txs } = await blockexplorer.getMultiAddress(addresses);
 
-    // channel.sendToQueue(queue, Buffer.from(JSON.stringify("{gia: true}")));
-    console.log(" [x] Sent everything");
-    // connection.close();
-  });
-  setTimeout(function() {
-    connection.close();
-    process.exit(0);
-  }, 10000);
-});
+      _.forEach(txs, (item, index) => {
+        channel.sendToQueue(queue, Buffer.from(JSON.stringify(item)));
+        console.log(" [x] Sent transaction in index: %d", index);
+      });
+
+      // channel.sendToQueue(queue, Buffer.from(JSON.stringify("{gia: true}")));
+      console.log(" [x] Sent everything");
+
+      return Promise.resolve();
+    } else {
+      return Promise.reject();
+    }
+  })
+  .catch(console.warn);
